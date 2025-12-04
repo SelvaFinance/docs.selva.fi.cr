0308d4d675ef2cd7f92b47b499cb8c820c90cd0d
Xavier Fernandez <xavier@edenia.com>
2025-12-04 11:01:16 -0600

    config: Update Docusaurus config for custom domain docs.selva.fi.cr
    - Changed url from https://selvafinance.github.io to https://docs.selva.fi.cr
    - Changed baseUrl from /docs.selva.fi.cr/ to / for custom domain setup
    - DNS and GitHub Pages custom domain are now active

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,10 +11,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://selvafinance.github.io',
+  url: 'https://docs.selva.fi.cr',
   // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs.selva.fi.cr/',
+  // For custom domain, baseUrl should be '/'
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   organizationName: 'SelvaFinance',
