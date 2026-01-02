ada365195042480e6adefe9bfa064ba60a5dc214
Leister Alvarado <leister.alvarado@gmail.com>
2026-01-02 12:14:23 -0600

    Add favicon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,42 @@ const path = require('path');
 const config = {
   title: 'Selva API Documentation',
   tagline: 'Soluciones Electrónicas Locales de Valor',
-  favicon: 'img/favicon.ico',
+  favicon: 'favicon.ico',
+  headTags: [
+    {
+      tagName: 'link',
+      attributes: {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png',
+      },
+    },
+    {
+      tagName: 'link',
+      attributes: {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon-32x32.png',
+      },
+    },
+    {
+      tagName: 'link',
+      attributes: {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon-16x16.png',
+      },
+    },
+    {
+      tagName: 'link',
+      attributes: {
+        rel: 'manifest',
+        href: '/site.webmanifest',
+      },
+    },
+  ],
 
   // Set the production url of your site here
   url: 'https://docs.selva.fi.cr',
